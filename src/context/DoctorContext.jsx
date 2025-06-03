a8c0e27ed7349e842a0af8859a461e147e5779b3
modit1802 <21803019@mail.jiit.ac.in>
2025-06-03 22:16:15 +0530

    changes again made in doctor

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL||import.meta.env.VITE_VERCEL_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([]);
     const [dashData, setDashData] = useState(false)
